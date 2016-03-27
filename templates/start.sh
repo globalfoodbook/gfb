2a70f694e5b5cbd80646bec899ff43ca43d16165
Ikenna Okpala <ikennaokpala@users.noreply.github.com>
2016-03-28 00:51:56 +0100

    Change owner after interpolation

--- templates/start.sh
+++ templates/start.sh
@@ -5,8 +5,6 @@ cp /home/gfb/wp-config.php /home/gfb/app/wp/wp-config.php
 cp /home/gfb/index-wp-redis.php /home/gfb/app/wp/index-wp-redis.php
 cp /home/gfb/index.php /home/gfb/app/wp/index.php
 
-sudo chown www-data:www-data  -R /home/gfb/app && sudo find /home/gfb/app -type d -exec chmod 755 {} \; && sudo find /home/gfb/app -type f -exec chmod 644 {} \;
-
 for name in MYSQL_ENV_MYSQL_DATABASE MYSQL_ENV_MYSQL_USER MYSQL_ENV_MYSQL_PASSWORD MYSQL_PORT_3306_TCP_ADDR MYSQL_PORT_3306_TCP_PORT
 do
     eval value=\$$name
@@ -18,6 +16,8 @@ do
     eval value=\$$name
     sed -i "s|\${${name}}|${value}|g" /home/gfb/app/wp/index-wp-redis.php
 done
+
+sudo chown www-data:www-data  -R /home/gfb/app && sudo find /home/gfb/app -type d -exec chmod 755 {} \; && sudo find /home/gfb/app -type f -exec chmod 644 {} \;
 
 sudo service php5-fpm start
 sudo service nginx start
