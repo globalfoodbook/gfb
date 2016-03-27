f1643fce2b2bfc2870bd366dfcd4feaed105f1ec
Ikenna Okpala <ikennaokpala@users.noreply.github.com>
2016-03-27 16:44:58 +0100

    Fix permissions

--- templates/start.sh
+++ templates/start.sh
@@ -5,6 +5,8 @@ cp /home/gfb/wp-config.php /home/gfb/app/wp/wp-config.php
 cp /home/gfb/index-wp-redis.php /home/gfb/app/wp/index-wp-redis.php
 cp /home/gfb/index.php /home/gfb/app/wp/index.php
 
+sudo chown www-data:www-data  -R /home/gfb/app && sudo find /home/gfb/app -type d -exec chmod 755 {} \; && sudo find /home/gfb/app -type f -exec chmod 644 {} \;
+
 for name in MYSQL_ENV_MYSQL_DATABASE MYSQL_ENV_MYSQL_USER MYSQL_ENV_MYSQL_PASSWORD MYSQL_PORT_3306_TCP_ADDR MYSQL_PORT_3306_TCP_PORT
 do
     eval value=\$$name
@@ -17,8 +19,8 @@ do
     sed -i "s|\${${name}}|${value}|g" /home/gfb/app/wp/index-wp-redis.php
 done
 
-sudo service php5-fpm restart
-sudo service nginx restart
+sudo service php5-fpm start
+sudo service nginx start
 
 touch /etc/nginx/logs/gfb/access.log /etc/nginx/logs/gfb/error.log
 tail -F /etc/nginx/logs/gfb/access.log /etc/nginx/logs/gfb/error.log
